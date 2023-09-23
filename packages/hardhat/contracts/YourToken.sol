99037c22a1ca36da20b3ae88f7ca872851e39e45
Justin <jrkrogers@gmail.com>
2023-09-22 23:48:00 -0400

    Added Token Factory, LSDWrap V1

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -2,11 +2,87 @@ pragma solidity 0.8.4; //Do not change the solidity version as it negativly impa
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
 
 // learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
 
-contract YourToken is ERC20 {
-  constructor() ERC20("Gold", "GLD") {
-    //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
-  }
+contract LSDWrap is ERC20, ERC20Wrapper {
+    address public factoryOwner;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        IERC20 underlyingToken,
+        address _factoryOwner
+    ) ERC20(name, symbol) ERC20Wrapper(underlyingToken) {
+        factoryOwner = _factoryOwner;
+    }
+
+    // Override decimals function to resolve ambiguity
+    function decimals() public view virtual override(ERC20, ERC20Wrapper) returns (uint8) {
+        return super.decimals();
+    }
+    
+    function wrap(uint256 amount) public {
+        depositFor(msg.sender, amount);
+    }
+
+    function unwrap(uint256 amount) public {
+        withdrawTo(msg.sender, amount);
+    }
 }
+
+contract TokenFactory is Ownable {
+    address[] public createdTokens;
+    IERC20[] public masterAllowedTokens; // Centralized array of allowed token addresses
+    bool public ownershipAssigned = false;
+
+    event MasterTokenAdded(IERC20 token);
+    event WrappedTokenCreated(address wrappedToken, IERC20 underlyingToken);
+
+    function addMasterAllowedToken(IERC20 token) public {
+        require(!isMasterAllowedToken(token), "Token already in master list");
+        if (ownershipAssigned) {
+            require(msg.sender == owner(), "Only the owner can add tokens");
+        }
+        masterAllowedTokens.push(token);
+        emit MasterTokenAdded(token);
+    }
+
+    function getMasterAllowedTokens() public view returns (IERC20[] memory) {
+        return masterAllowedTokens;
+    }
+
+    function createWrappedToken(
+        string memory name,
+        string memory symbol,
+        IERC20 selectedUnderlyingToken
+    ) public returns (address) {
+        require(isMasterAllowedToken(selectedUnderlyingToken), "Token not in master allowed list");
+        LSDWrap newToken = new LSDWrap(name, symbol, selectedUnderlyingToken, msg.sender);
+        createdTokens.push(address(newToken));
+        emit WrappedTokenCreated(address(newToken), selectedUnderlyingToken);
+        return address(newToken);
+    }
+
+    function isMasterAllowedToken(IERC20 token) internal view returns (bool) {
+        for (uint i = 0; i < masterAllowedTokens.length; i++) {
+            if (masterAllowedTokens[i] == token) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getCreatedTokens() public view returns (address[] memory) {
+        return createdTokens;
+    }
+    
+    // Override the transferOwnership function to set ownershipAssigned to true
+    function transferOwnership(address newOwner) public override onlyOwner {
+        super.transferOwnership(newOwner);
+        ownershipAssigned = true;
+    }
+}
+
--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -1,3 +1,220 @@
-const deployedContracts = null;
+const contracts = {
+  31337: [
+    {
+      chainId: "31337",
+      name: "localhost",
+      contracts: {
+        TokenFactory: {
+          address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+          abi: [
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "contract IERC20",
+                  name: "token",
+                  type: "address",
+                },
+              ],
+              name: "MasterTokenAdded",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: true,
+                  internalType: "address",
+                  name: "previousOwner",
+                  type: "address",
+                },
+                {
+                  indexed: true,
+                  internalType: "address",
+                  name: "newOwner",
+                  type: "address",
+                },
+              ],
+              name: "OwnershipTransferred",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "address",
+                  name: "wrappedToken",
+                  type: "address",
+                },
+                {
+                  indexed: false,
+                  internalType: "contract IERC20",
+                  name: "underlyingToken",
+                  type: "address",
+                },
+              ],
+              name: "WrappedTokenCreated",
+              type: "event",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "contract IERC20",
+                  name: "token",
+                  type: "address",
+                },
+              ],
+              name: "addMasterAllowedToken",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "string",
+                  name: "name",
+                  type: "string",
+                },
+                {
+                  internalType: "string",
+                  name: "symbol",
+                  type: "string",
+                },
+                {
+                  internalType: "contract IERC20",
+                  name: "selectedUnderlyingToken",
+                  type: "address",
+                },
+              ],
+              name: "createWrappedToken",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              name: "createdTokens",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "getCreatedTokens",
+              outputs: [
+                {
+                  internalType: "address[]",
+                  name: "",
+                  type: "address[]",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "getMasterAllowedTokens",
+              outputs: [
+                {
+                  internalType: "contract IERC20[]",
+                  name: "",
+                  type: "address[]",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              name: "masterAllowedTokens",
+              outputs: [
+                {
+                  internalType: "contract IERC20",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "owner",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "ownershipAssigned",
+              outputs: [
+                {
+                  internalType: "bool",
+                  name: "",
+                  type: "bool",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "renounceOwnership",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "newOwner",
+                  type: "address",
+                },
+              ],
+              name: "transferOwnership",
+              outputs: [],
+              stateMutability: "nonpayable",
+              type: "function",
+            },
+          ],
+        },
+      },
+    },
+  ],
+} as const;
 
-export default deployedContracts;
+export default contracts;
